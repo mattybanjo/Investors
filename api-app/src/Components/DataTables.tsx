3b850e0716c83934fede511466e59b720206cbd5
mattybanjo <mattbanjo8@gmail.com>
2024-04-10 18:31:20 +0100

    Update DataTables.tsx

--- api-app/src/Components/DataTables.tsx
+++ api-app/src/Components/DataTables.tsx
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from "react";
 import InvestorsTable from "./InvestorsTable";
+import InvestorDetails from "./InvestorDetails";
+import Investor from "../Entity/Investor";
 
 function DataTables() {
+    const [investorSelected, setInvestorSelected] = useState(false);
+    const [selectedInvestor, setSelectedInvestor] = useState({} as Investor);
+
+    function investorSelect(): void {
+        setInvestorSelected(true);
+    }
+
+    function selectInvestor(investor: Investor): void {
+        setSelectedInvestor(investor);
+        setInvestorSelected(true);
+    }
 
     return (
         <div>
-             <InvestorsTable/>
+            {investorSelected ? (
+                <div>
+                    <InvestorDetails investor={selectedInvestor} />
+                </div>
+            ) : (
+                <InvestorsTable selectInvestor={selectInvestor} investorSelect={investorSelect} />
+            )}
+
         </div>
     );
 }
