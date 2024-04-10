84cb3a80f9b1528b93ec6b504d5a2e9fd616ccc1
mattybanjo <mattbanjo8@gmail.com>
2024-04-10 18:28:53 +0100

    Create DataTables.tsx

---
+++ api-app/src/Components/DataTables.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import InvestorsTable from "./InvestorsTable";
+
+function DataTables() {
+
+    return (
+        <div>
+             <InvestorsTable/>
+        </div>
+    );
+}
+
+export default DataTables;
