dabb9e5e0efb4fdf287c20a4c0cf6a4a4a7b8523
mattybanjo <mattbanjo8@gmail.com>
2024-04-11 04:53:21 +0100

    Update InvestorDetails.tsx

--- api-app/src/Components/InvestorDetails.tsx
+++ api-app/src/Components/InvestorDetails.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import Investor from "../Entity/Investor";
-// import { Container, Dropdown, DropdownButton, DropdownItem } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
-import DropdownItem from "react-bootstrap/DropdownButton";
-import Container from "react-bootstrap/Container";
+import Investor from "../Entity/Investor";
 import CommitmentsTable from "./Commitments";
+// import { Container, Dropdown, DropdownButton, DropdownItem } from "react-bootstrap";
 
 
 type InvestorDetailsProps = {
@@ -14,11 +14,29 @@ type InvestorDetailsProps = {
 
 function InvestorDetails(props: InvestorDetailsProps) {
     const assetClasses = ["pe", "pd", "re", "inf", "nr", "hf"];
+    const assetClassesNames = [
+        "Primary Equity",
+        "Private Debt",
+        "Real Estate",
+        "Infrastructure",
+        "Natural Resources",
+        "Hedge Funds"
+    ]
+    // const assetClasses = new Map([
+    //     ["Primary Equity", "pe"],
+    //     ["Private Debt", "re"],
+    //     ["Real Estate", "re"],
+    //     ["Infrastructure", "inf"],
+    //     ["Natural Resources", "nr"],
+    //     ["Hedge Funds", "hf"]
+    // ]);
     const [assetClass, setAssetClass] = useState("");
+    const [assetClassName, setAssetClassName] = useState("");
     const [assetSelected, setAssetSelected] = useState(false);
 
-    function selectAssetClass(asset: string) {
-        setAssetClass(asset);
+    function selectAssetClass(index: number) {
+        setAssetClass(assetClasses[index]);
+        setAssetClassName(assetClassesNames[index]);
         setAssetSelected(true);
     }
 
@@ -26,7 +44,7 @@ function InvestorDetails(props: InvestorDetailsProps) {
         <div className="InvestorDetails">
             <h2>{props.investor.firm_name.toUpperCase()} Details</h2>
             <Container id="asset-dropdown">
-                <DropdownButton id="asset-selector" title="Asset Type" menuVariant="dark">
+                {/* <DropdownButton id="asset-selector" title="Asset Type" menuVariant="dark">
                     <Dropdown.Item onClick={() => selectAssetClass(assetClasses[0])} >
                         Private Equity
                     </Dropdown.Item>
@@ -45,11 +63,27 @@ function InvestorDetails(props: InvestorDetailsProps) {
                     <Dropdown.Item onClick={() => selectAssetClass(assetClasses[5])}>
                         Hedge Funds
                     </Dropdown.Item>
+                </DropdownButton> */}
+
+                {/* <DropdownButton id="asset-selector" title="Asset Type" menuVariant="dark">
+                    {Object.keys(assetClasses).map((key, index) => (
+                        <Dropdown.Item key={index} onClick={() => selectAssetClass(key)}>
+                            {key}
+                        </Dropdown.Item>
+                    ))}
+                </DropdownButton> */}
+                <DropdownButton id="asset-selector" title="Asset Type" menuVariant="dark">
+                    {assetClassesNames.map((key, index) => (
+                        <Dropdown.Item key={index} onClick={() => selectAssetClass(index)}>
+                            {key}
+                        </Dropdown.Item>
+                    ))}
                 </DropdownButton>
             </Container>
 
             <Container id="investor-details-table">
-                <table>
+                {/* <table> */}
+                <Table>
                     <thead>
                         <tr>
                             <th>Firm ID</th>
@@ -66,14 +100,16 @@ function InvestorDetails(props: InvestorDetailsProps) {
                     </tr>
                     <tbody>
                     </tbody>
-                </table>
+                </Table>
+                {/* </table> */}
             </Container>
-
-
 
             {assetSelected && (
                 <Container id="commitments">
-                    <CommitmentsTable investor_id={props.investor.firm_id} asset_class={assetClass} />
+                    <CommitmentsTable 
+                    investor_id={props.investor.firm_id} 
+                    asset_class={assetClass} 
+                    asset_class_name={assetClassName} />
                 </Container>
             )}
         </div>
