78c02f7551507257bcd91eccb772e9b8006a4f22
mattybanjo <mattbanjo8@gmail.com>
2024-04-11 04:57:21 +0100

    Update Commitments.tsx

--- api-app/src/Components/Commitments.tsx
+++ api-app/src/Components/Commitments.tsx
@@ -1,52 +1,52 @@
-import React, { useEffect, useState }from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import Commitment from "../Entity/Commitment";
 import axios from "axios";
 import https from "https";
-import Commitment from "../Entity/Commitment";
 
 type CommitmentsTableProps = {
     investor_id: number,
     asset_class: string
+    asset_class_name: string;
 }
-
-
 
 function CommitmentsTable(props: CommitmentsTableProps) {
     const [data, setData] = useState([] as Commitment[]);
 
     async function getCommitments(): Promise<any> {
         const url = `http://localhost:8000/api/investor/commitment/${props.asset_class}/${props.investor_id}`;
-    
+
         const httpsAgent = new https.Agent({
             maxVersion: 'TLSv1.3',
             minVersion: 'TLSv1.2'
         });
-    
+
         try {
             const response = await axios.get(url, {
                 httpsAgent: httpsAgent
             });
-    
+
             setData(response.data as Commitment[]);
         } catch (error) {
             console.error('Error:', error);
         }
     }
 
-    useEffect( () => {
+    useEffect(() => {
         getCommitments();
     }, [props.asset_class]);
 
-    return(
+    return (
         <div className="CommitmentsTable">
-            <h2>Commitments</h2>
-            <Container id="commitments-table">
-                <table>
+            <h2>{props.asset_class_name} Commitments</h2>
+            <Container id="commitments-type-table">
+                <Table>
                     <thead>
                         <tr>
                             <th>Commitment ID</th>
-                            <th>Asset Class</th>
-                            <th>Firm ID</th>
+                            {/* <th>Firm ID</th> */}
+                            {/* <th>Asset Class</th> */}
                             <th>Currency</th>
                             <th>Amount</th>
                         </tr>
@@ -55,14 +55,14 @@ function CommitmentsTable(props: CommitmentsTableProps) {
                         {data.map((asset, index) => (
                             <tr key={index}>
                                 <td>{asset.id}</td>
-                                <td>{asset.asset_class}</td>
-                                <td>{asset.firm_id}</td>
+                                {/* <td>{asset.firm_id}</td> */}
+                                {/* <td>{asset.asset_class}</td> */}
                                 <td>{asset.currency}</td>
                                 <td>{asset.amount}</td>
                             </tr>
                         ))}
                     </tbody>
-                </table>
+                </Table>
             </Container>
         </div>
     );
